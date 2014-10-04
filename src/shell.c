5999549ff35784483205e184ba3948d99a71d744
C.W. Hsiao <cwhsiao.lionel@gmail.com>
2014-10-04 19:50:52 +0800

    test: fix SYS_OPEN fail by creating output folder
    Signed-off-by: C.W. Hsiao <cwhsiao.lionel@gmail.com>

--- src/shell.c
+++ src/shell.c
@@ -145,7 +145,7 @@ void test_command(int n, char *argv[]) {
     int error;
 
     fio_printf(1, "\r\n");
-
+    host_action(SYS_SYSTEM, "mkdir -p output/");
     handle = host_action(SYS_OPEN, "output/syslog", 8);
     if(handle == -1) {
         fio_printf(1, "Open file error!\n\r");
